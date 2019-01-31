7fc1e8fe4f7e564277292cf1bc7403a048dcfe82
Mallarie <mallariemilano@gmail.com>
2019-01-30 16:30:53 -0800

    all non stretch goals completed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import headsTails from './heads-tails.js';
 
 const button = document.getElementById('coin-button');
 const image = document.getElementById('coin-image');
+const result = document.getElementById('result');
+const headCount = document.getElementById('head-count');
+const tailCount = document.getElementById('tail-count');
+let head = 0;
+let tail = 0;
 
 
 button.addEventListener('submit', function(event) {
@@ -13,10 +18,18 @@ button.addEventListener('submit', function(event) {
    
     if(side === 'heads') {
         image.src = './assets/heads.png';
+        result.textContent = 'heads!!!!!!!';
+        head++;
+        headCount.textContent = 'Head Count: ' + head; 
+
     }
     
     else {
         image.src = './assets/tails.jpg';
+        result.textContent = 'tails...';
+        tail++;
+        tailCount.textContent = 'Tail Count: ' + tail; 
+
     }
 
     image.classList.remove('hidden');
